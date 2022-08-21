7669e069025683a72a8599f01b6977e7e949e027
gzeiniss <gene_zeiniss@intuit.com>
2022-08-21 10:51:33 +0300

    Initial commit

---
+++ src/main/java/com/genezeiniss/jooq_liquibase_e2e/JooqLiquibaseE2eApplication.java
@@ -0,0 +1,13 @@
+package com.genezeiniss.jooq_liquibase_e2e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqLiquibaseE2eApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooqLiquibaseE2eApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/genezeiniss/jooq_liquibase_e2e/JooqLiquibaseE2eApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genezeiniss.jooq_liquibase_e2e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqLiquibaseE2eApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
